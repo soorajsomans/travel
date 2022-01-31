3a77fef3d82f03f44f9daf05e6f2edc7d34ac375
Sooraj Soman <soorajsomans@gmail.com>
2022-01-31 20:15:54 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './screens/places_list_screen.dart';
+import './providers/great_places.dart';
+
+void main() => runApp(Travel());
+
+class Travel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+      ),
+    );
+  }
+}
